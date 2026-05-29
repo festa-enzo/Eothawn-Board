1d06a52fe597ba792542db046bff47d0b0ffda23
Enzo <enzodfesta@gmail.com>
2026-05-29 15:46:29 -0300

    Consertar formatação dos cards

--- front-end/script.js
+++ front-end/script.js
@@ -1,8 +1,6 @@
 const botao = document.getElementById("btnNovaTarefa");
 const formulario = document.getElementById("formulario");
-const tarefa = document.getElementById("tarefa").value;
-const dia = document.getElementById("dia").value;
-const horario = document.getElementById("horario").value;
+const form = document.querySelector("form");
 
 
 botao.addEventListener("click", () => {
@@ -15,6 +13,32 @@ botao.addEventListener("click", () => {
 
 });
 
-console.log(tarefa);
-console.log(dia);
-console.log(horario);
+form.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const tarefa = document.getElementById("tarefa").value;
+    const dia = document.getElementById("dia").value;
+    const horario = document.getElementById("horario").value;
+
+    const card = document.createElement("div");
+
+    card.textContent = `${horario} - ${tarefa}`;
+
+    card.classList.add("card");
+
+    const listas = {
+        "Segunda": "segunda-lista",
+        "Terça": "terca-lista",
+        "Quarta": "quarta-lista",
+        "Quinta": "quinta-lista",
+        "Sexta": "sexta-lista",
+        "Sábado": "sabado-lista",
+        "Domingo": "domingo-lista"
+    };
+
+    const coluna = document.getElementById(listas[dia]);
+
+    coluna.appendChild(card);
+
+});
